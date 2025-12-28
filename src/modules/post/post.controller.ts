9dca1794148696a3b67182f7d5d8182b27c02eb5
TonmoyZohani <tonmoyzohani@gmail.com>
2025-12-28 22:56:52 +0600

    service complete

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -1,7 +1,13 @@
 import { Request, Response } from "express";
+import { postService } from "./post.service";
 
 const createPost = async (req: Request, res: Response) => {
-  console.log({ req, res });
+  try {
+    const result = await postService.createPost(req.body);
+    res.status(201).json(result);
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 };
 
 export const postController = {
