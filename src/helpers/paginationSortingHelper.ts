0a33871f1dfda1d7dee7ce89607098a98d2691a1
TonmoyZohani <tonmoyzohani@gmail.com>
2026-01-07 00:26:00 +0600

    pagination helper function done

---
+++ src/helpers/paginationSortingHelper.ts
@@ -0,0 +1,33 @@
+type IOptions = {
+  page?: number;
+  limit?: number;
+  sortBy?: string;
+  sortOrder?: string;
+};
+
+type IOptionsResult = {
+  page: number;
+  limit: number;
+  skip: number;
+  sortBy: string;
+  sortOrder: string;
+};
+
+const paginationSortingHelper = (options: IOptions): IOptionsResult => {
+  const page: number = Number(options.page ?? 1);
+  const limit: number = Number(options.limit ?? 10);
+  const skip: number = (page - 1) * limit;
+
+  const sortBy: string = (options.sortBy as string) ?? "createdAt";
+  const sortOrder: string = (options.sortOrder as string) ?? "desc";
+
+  return {
+    page,
+    limit,
+    skip,
+    sortBy,
+    sortOrder,
+  };
+};
+
+export default paginationSortingHelper;
--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { postService } from "./post.service";
 import { Post, PostStatus } from "../../../generated/prisma/client";
+import paginationSortingHelper from "../../helpers/paginationSortingHelper";
 
 const createPost = async (req: Request, res: Response) => {
   try {
@@ -24,11 +25,14 @@ const getAllPost = async (req: Request, res: Response) => {
       : undefined;
     const status = req.query.status as PostStatus | undefined;
     const authorId = req.query.authorId as string | undefined;
-    const page = Number(req.query.page ?? 1);
-    const limit = Number(req.query.limit ?? 10);
-    const skip = (page - 1) * limit;
-    const sortBy = req.query.sortBy as string | undefined;
-    const sortOrder = req.query.sortOrder as string | undefined;
+    // const page = Number(req.query.page ?? 1);
+    // const limit = Number(req.query.limit ?? 10);
+    // const sortBy = req.query.sortBy as string | undefined;
+    // const sortOrder = req.query.sortOrder as string | undefined;
+
+    const { page, limit, sortBy, sortOrder } = paginationSortingHelper(
+      req.query
+    );
 
     const result = await postService.getAllPost({
       search,
@@ -38,7 +42,6 @@ const getAllPost = async (req: Request, res: Response) => {
       authorId,
       page,
       limit,
-      skip,
       sortBy,
       sortOrder,
     });
