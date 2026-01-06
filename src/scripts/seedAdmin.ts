1695d1adf0ac979502acbb3fc65a0547e09ff331
TonmoyZohani <tonmoyzohani@gmail.com>
2026-01-06 22:34:52 +0600

    see admin creation first part done

---
+++ src/scripts/seedAdmin.ts
@@ -0,0 +1,35 @@
+import { prisma } from "../lib/prisma";
+import { UserRole } from "../middlewares/auth";
+
+async function seedAdmin() {
+  try {
+    const adminData = {
+      name: "HR Admin",
+      email: "hr@admin.com",
+      password: "admin",
+      role: UserRole.ADMIN,
+    };
+
+    const existingUser = await prisma.user.findUnique({
+      where: {
+        email: adminData.email,
+      },
+    });
+
+    if (existingUser) {
+      throw new Error("Admin already exists");
+    }
+
+    const signUpAdmin = await fetch("http://localhost:3000/api/auth/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(adminData),
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+seedAdmin();
